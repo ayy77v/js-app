cc9aa36f672a3bd83cbb5dc7b4314add482c583c
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2019-12-11 20:30:47 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports={
+	mode: "development",
+	devServer: {
+		contentBase: "./dist"
+	},
+	module: {
+		rules:[ 
+		{
+			test: /\.js$/,
+			use: [{ loader: "babel-loader"}],
+			exclude: /node_modules/
+		}]
+	}
+}
